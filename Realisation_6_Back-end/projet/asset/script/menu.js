44d1f11bb1e16e753a38dd36d362203915200450
Priscillien herbet <priscillien.herbet@gmail.com>
2022-06-17 10:56:07 +0200

    update ppt

--- Realisation_6_Back-end/projet/asset/script/menu.js
+++ Realisation_6_Back-end/projet/asset/script/menu.js
@@ -1,4 +1,6 @@
-const button_menu = document.querySelector('#button_menu') ;
+// on récupère l'action de clique sur le bouton du mennu
+const button_menu = document.querySelector('#button_menu');
+// on récupère la classe a changer dans une constante
 const nav = document.querySelector('.hide_menu');
 
 button_menu.addEventListener('click', function(event)
